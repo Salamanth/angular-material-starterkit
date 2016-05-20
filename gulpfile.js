79ec4f8a2a81e5c0aef5015efbc420e5f04ee4c7
Hadrien BRUNAUD <hadrien.brunaud@gmail.com>
2016-05-20 14:35:28 +0200

    Update and rename gulpfile.json to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 // Dependencies
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 var sass = require('gulp-sass');
 
 // Style task
-gulp.task('styles', function() {
+gulp.task('default', function() {
   gulp.src('static/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(concat('app.min.css'))
     .pipe(gulp.dest('./static/css/'));
 });
 
